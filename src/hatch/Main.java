2c3c6c344daa9514bea1169a3846f78f07afc8ba
Chatch520 <chatch520@gmail.com>
2021-01-29 14:57:21 -0700

    added a semicolon where needed

--- src/hatch/Main.java
+++ src/hatch/Main.java
@@ -3,6 +3,6 @@ package hatch;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello From Chase")
+        System.out.println("Hello From Chase");
     }
 }
